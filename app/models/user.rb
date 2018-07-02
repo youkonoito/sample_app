1e7d53ca3529776993e7fa13c69e166c3238587e
EC2 Default User <ec2-user@ip-172-31-20-104.us-east-2.compute.internal>
2018-07-02 07:09:31 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
